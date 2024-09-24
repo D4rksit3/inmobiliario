120e7a69ece7592431b296af1cd94a093f9b2d66
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2024-09-23 19:59:38 -0500

    main

--- create_user.php
+++ create_user.php
@@ -1,13 +1,18 @@
+
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Procesar formulario si se envía
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include("db.php"); // Incluir el archivo de conexión a la base de datos
 
+    $nombre = $_POST['nombre'];
     $correo = $_POST['correo'];
-    $contrasena_plana = $_POST['contrasena'];
+    $contrasena_plana = $_POST['contrasena']
     $contrasena_cifrada = password_hash($contrasena_plana, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO usuarios (correo, contrasena) VALUES (?, ?)";
+    $sql = "INSERT INTO usuarios (nombre, correo, contrasena) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss", $correo, $contrasena_cifrada);
 
@@ -24,6 +29,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h2>Crear Usuario</h2>
 <form action="dashboard.php?page=create_user" method="POST">
+    <label for="nombre">Nombre:</label>
+    <input type="nombre" id="nombre" name="nombre" required>
     <label for="correo">Correo:</label>
     <input type="email" id="correo" name="correo" required>
 
--- procesar.php
+++ procesar.php
@@ -1,11 +1,11 @@
 <?php
-ini_set('display_errors', 1);
+/* ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+ */
 
 session_start();
-include("db.php"); // Incluye tu archivo de conexión a la base de datos
+include("db.php"); 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $correo = $_POST['correo'];
