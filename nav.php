a23d12e77c4b33d8635e815ceb02f7933b848994
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2024-09-23 19:52:28 -0500

    main

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
   
   <head>
   <meta charset="utf-8">
-  <title>EstateAgency Bootstrap Template</title>
+  <title>Sol de oro</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
@@ -41,7 +41,7 @@
         <span></span>
         <span></span>
       </button>
-      <a class="navbar-brand text-brand" href="index.html">Agencia<span class="color-b">Inmobiliaria</span></a>
+      <a class="navbar-brand text-brand" href="index.html">Sol de<span class="color-b">Oro</span></a>
       <button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse"
         data-target="#navbarTogglerDemo01" aria-expanded="false">
         <span class="fa fa-search" aria-hidden="true"></span>
