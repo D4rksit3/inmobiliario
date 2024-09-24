2b08d44f997cc834de8c9c03e3e7db800c75f759
Juan Roque <50809035+D4rksit3@users.noreply.github.com>
2024-09-23 19:52:52 -0500

    main

--- nav.php
+++ nav.php
@@ -41,7 +41,7 @@
         <span></span>
         <span></span>
       </button>
-      <a class="navbar-brand text-brand" href="index.html">Sol de<span class="color-b">Oro</span></a>
+      <a class="navbar-brand text-brand" href="index.html">Sol de<span class="color-b"> Oro</span></a>
       <button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse"
         data-target="#navbarTogglerDemo01" aria-expanded="false">
         <span class="fa fa-search" aria-hidden="true"></span>
